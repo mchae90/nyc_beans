9924d1a297a68a22c1aa7af8724ecfbd8218e8e2
mchae90 <mchae90@gmail.com>
2025-12-01 01:00:52 -0500

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for PWA
-const CACHE_NAME = 'nyc-restaurants-v1.0.4';
+const CACHE_NAME = 'nyc-restaurants-v1.0.5';
 const urlsToCache = [
   '/nyc_beans/',
   '/nyc_beans/index.html',
